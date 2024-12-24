ab1b4f4478513cb840412bf32b6db126415c7196
youngs01 <124503790+youngs01@users.noreply.github.com>
2024-12-24 11:51:06 +0500

    this is pushed by git in vs code

---
+++ limlydraw/src/main/java/com/limlydraw/LimlydrawApplication.java
@@ -0,0 +1,13 @@
+package com.limlydraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimlydrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimlydrawApplication.class, args);
+	}
+
+}
---
+++ limlydraw/src/test/java/com/limlydraw/LimlydrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limlydraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimlydrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
